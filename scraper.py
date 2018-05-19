72ab7bbcc91d92369d638dfc6b1e6aa07af59b3b
nobisseb <nobissebastian@gmail.com>
2018-05-20 01:59:33 +0200

    untercategory klassen hinzugefügt

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ Hier eintragen!
 import urllib.request
 from bs4 import BeautifulSoup
 
+
 class Scraper:
     def __init__(self):
         self.raw_data = {}
@@ -13,18 +14,32 @@ class Scraper:
 class MobileScraper(Scraper):
     def __init__(self):
         super().__init__()
+        self.list_top_level_categories = []
+        self.top_level_category_urls = {"lkw_ueber_7.5t": "https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=ALSO_DAMAGE_UNREPAIRED&grossPrice=false&isSearchRequest=true&maxPowerAsArray=PS&minPowerAsArray=PS&scopeId=TO75", "sattelzugmaschinen": "https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=ALSO_DAMAGE_UNREPAIRED&grossPrice=false&isSearchRequest=true&maxPowerAsArray=PS&minPowerAsArray=PS&scopeId=STT"}
 
     def fetch_raw_data(self):
-        top_level_category_urls = {"lkw_ueber_7.5t": "https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=ALSO_DAMAGE_UNREPAIRED&grossPrice=false&isSearchRequest=true&maxPowerAsArray=PS&minPowerAsArray=PS&scopeId=TO75", "sattelzugmaschinen": "https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=ALSO_DAMAGE_UNREPAIRED&grossPrice=false&isSearchRequest=true&maxPowerAsArray=PS&minPowerAsArray=PS&scopeId=STT"}
+        for category in self.top_level_category_urls:
+            self.list_top_level_categories.append(MobileTopLevelCategory(category, self.top_level_category_urls[category]))
 
-        for category in top_level_category_urls:
-            try:
-                url = top_level_category_urls[category]
-            except urllib.URLError as e:
-                print("The server to :" + url + "could not be found!")
-            except urllib.HTTPError as e:
-                print(e)
-            else:
-                req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'})
-                html_code = urllib.request.urlopen(req).read()
-                print(html_code)
+        for category in self.list_top_level_categories:
+            category.request_html_code()
+
+
+class MobileTopLevelCategory:
+    def __init__(self, category_name: str, url: str):
+        self.category_name = category_name
+        self.url = url
+        self.html_code = ""
+
+    def request_html_code(self):
+        try:
+            req = urllib.request.Request(self.url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'})
+            handler = urllib.request.urlopen(req)
+
+        except urllib.request.HTTPError as e:
+            raise ConnectionError(str(e))
+        except urllib.request.URLError as e:
+            raise ConnectionError("The server to : " + self.url + " could not be found!")
+
+        else:
+            self.html_code = handler.read()
